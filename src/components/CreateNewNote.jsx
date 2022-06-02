62c0e8522e54fd842b17ecd308406e0cfd0085f6
KhomotsoTibane <jledoza@gmail.com>
2022-06-02 02:39:25 +0200

    create note section

---
+++ src/components/CreateNewNote.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+import AddIcon from '@mui/icons-material/Add';
+
+
+function CreateNewNote(props) {
+
+  const [note, setNote]=useState({
+    title:"",
+    content:""
+  });
+
+  function handleNoteInput(event){
+    const{name, value} = event.target;
+
+    setNote(prevNote=>{
+      return{
+        ...prevNote, 
+        [name]:value
+      };
+    });
+  }
+
+  function HandleSubmitNote(event){
+    props.onAdd(note);
+    setNote({
+      title:"",
+      content:""
+    });
+    event.preventDefault()
+  }
+
+  return (
+    <div>
+        <form className="create-note">
+            <input name="title" onChange={handleNoteInput} value={note.title}placeholder="title"/>
+            <textarea name="content" onChange={handleNoteInput} value={note.content}placeholder="Write note...." rows="3"/>
+            <button onClick={HandleSubmitNote}>
+              <AddIcon />
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default CreateNewNote
