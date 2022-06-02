c1bc62c965b030e2ea2858386332dc98e59801c6
KhomotsoTibane <jledoza@gmail.com>
2022-06-02 02:38:57 +0200

    included add and delete functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,40 @@
-import React from "react"
+import React,{useState} from "react"
 import Header from "./Header"
 import Note from "./Note"
 import Footer from "./Footer"
+import CreateNewNote from "./CreateNewNote"
 
 function App() {
+
+ const [noteCollection, setNoteCollection] = useState([]);
+
+ function addNote(newNote) {
+   setNoteCollection( prevNoteCollection => {
+    return [...prevNoteCollection, newNote];
+  });
+ }
+
+ function deleteNote(id){
+  setNoteCollection(prevNoteCollection =>{
+   return prevNoteCollection.filter((noteItem, index)=>{
+      return index !== id;
+    });
+  });
+ }
+
   return (
     <div>
       <Header/>
-      <Note/>
+      <CreateNewNote onAdd={addNote} onDelete={deleteNote} />
+      {noteCollection.map((noteItem, index)=>{
+        return <Note
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
+        />
+      })}
       <Footer/>
     </div>
   )
