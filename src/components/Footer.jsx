99904b520bc2dfa7ea73db6606ceec62069e5c29
KhomotsoTibane <jledoza@gmail.com>
2022-06-03 01:21:10 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright © {currentYear}</p>
+      <p> © khomotso Tibane {currentYear}</p>
     </footer>
   );
 }
